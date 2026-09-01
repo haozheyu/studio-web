5231f38b199c089c59b6ef07571935b432f3d464
haozheyu <1216108213@qq.com>
2026-09-02 01:26:43 +0800

    fix: type global window helper in Vue templates

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,3 +5,9 @@ declare module '*.vue' {
   const component: DefineComponent<object, object, any>
   export default component
 }
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    window: Window
+  }
+}
